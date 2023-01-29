fea21b34ba90f820626daea75b3aa3566ee56b45
Rosario Henry <104395752+HenryRosario@users.noreply.github.com>
2023-01-29 19:52:54 -0400

    Add files via upload
    Registro de ocupaciones

---
+++ Registro_Ocupacion/Migrations/20230128010133_Inicial.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace RegistroOcupacion.Migrations
+{
+    /// <inheritdoc />
+    public partial class Inicial : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Ocupaciones",
+                columns: table => new
+                {
+                    OcupacionesId = table.Column<int>(type: "INTEGER", nullable: false)
+                        .Annotation("Sqlite:Autoincrement", true),
+                    Descripcion = table.Column<string>(type: "TEXT", nullable: true),
+                    Salario = table.Column<double>(type: "REAL", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Ocupaciones", x => x.OcupacionesId);
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Ocupaciones");
+        }
+    }
+}
